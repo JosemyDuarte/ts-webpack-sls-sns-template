82290ccffb54cf3436e38dabed9e6109e8f78720
Josemy Duarte <duartejosemy@gmail.com>
2020-07-18 20:54:29 +0200

    Added build command to npm

--- src/listener.ts
+++ src/listener.ts
@@ -1,7 +1,6 @@
 import { SNSHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-
 export const listen: SNSHandler = async (event, _context) => {
   console.log('****************Message received****************');
   console.log(event);
